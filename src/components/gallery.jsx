8f791df05c2e1957a423b4742e8b3d836f335c1d
Jeff Dyer <jeffdyer@acm.org>
2022-09-02 15:39:46 -0700

    Update gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -5,15 +5,11 @@ const files = [
   },
   {
     source:
-      'https://cdn.acx.ac/l1aFe7vRi5oIe4g6QCL.png',
-  },
-  {
-    source:
       'https://cdn.acx.ac/o5dSVpbziL5fbz8mBCA.png',
   },
   {
     source:
-      'https://cdn.acx.ac/JePIZQrlSRaHA7AAmHp.png',
+      'https://cdn.acx.ac/MxRF0xqwSJXSqmmv4I9.png',
   },
   {
     source:
@@ -21,9 +17,8 @@ const files = [
   },
   {
     source:
-      'https://cdn.acx.ac/nKpHNjO3tgZtVxy8gU4.png',
+      'https://cdn.acx.ac/l1aFe7vRi5oIe4g6QCL.png',
   },
-  // More files...
 ]
 
 export default function Example() {
@@ -31,7 +26,7 @@ export default function Example() {
     <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
       {files.map((file) => (
         <li key={file.source} className="relative">
-          <div className="group block w-full overflow-hidden rounded-none bg-white focus-within:ring-2 focus-within:ring-gray-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+          <div className="group block w-full overflow-hidden rounded-none bg-white hover:ring-1 hover:ring-gray-400">
             <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
             <button type="button" className="absolute inset-0 focus:outline-none">
               <span className="sr-only">View details for {file.title}</span>
