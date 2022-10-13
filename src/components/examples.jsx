54a5e3b5773689dd978b3a8fe922f886a62db6ff
Jeff Dyer <jeffdyer@acm.org>
2022-10-13 13:04:45 -0700

    Style module

--- src/components/examples.jsx
+++ src/components/examples.jsx
@@ -1,7 +1,7 @@
 import Clock from './clock';
 import Counter from './counter';
 import Hello from './hello';
-import L104Form from './forms/L104/src/form.jsx';
+import L114Form from './forms/L114/src/Form.jsx';
 const Examples = () => {
   return (
     <div style={{ marginBottom: 10 }}>
