bede6c35ed3bca4c8a8a51b74c98412f00ea94b8
Jeff Dyer <jeffdyer@acm.org>
2023-08-24 11:32:35 -0700

    Hide menus

--- src/pages/tasks.jsx
+++ src/pages/tasks.jsx
@@ -34,8 +34,8 @@ const navigation = [
   { name: 'Languages', href: '/languages', current: false },
   { name: 'Tasks', href: '/tasks', current: true },
   { name: 'Compiles', href: '/compiles', current: false },
-  { name: 'Explorer', href: '/explorer', current: false },
-  { name: 'Settings', href: '/settings', current: false },
+//  { name: 'Explorer', href: '/explorer', current: false },
+//  { name: 'Settings', href: '/settings', current: false },
 ];
 
 function classNames(...classes) {
