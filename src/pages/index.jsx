58faaca36879e3e21138e796b47e095e4185cd08
Jeff Dyer <jeffdyer@acm.org>
2023-10-16 13:34:18 -0700

    Fix lint

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { getTitle } from '../lib/utils';
+import Link from 'next/link';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -12,7 +13,7 @@ export default function Compiles() {
   return (
     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       Welcome to the { getTitle() } console. Get started by navigating to&nbsp; 
-      <a href="/languages"><b>Languages</b></a> tab, selecting a language, and creating
+      <Link href="/languages"><b>Languages</b></Link> tab, selecting a language, and creating
       a task.
     </div>
   );
