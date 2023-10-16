3c2826d64bd85e710554591ebb105d365b5787b2
Jeff Dyer <jeffdyer@acm.org>
2023-10-16 16:16:15 -0700

    Add explorer and specs pages

---
+++ src/pages/specs.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { getTitle } from '../lib/utils';
+import Link from 'next/link';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Compiles() {
+  useEffect(() => {
+    document.title = getTitle();
+  }, []);
+  return (
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      Here is were was put the language specs.
+    </div>
+  );
+}
