6da3ff43839c624c40a3cdc183256d1dd8dfde37
Jeff Dyer <jeffdyer@acm.org>
2023-10-17 17:07:58 -0700

    debugging

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
 export function getTitle() {
   const hostnameParts = typeof document === "undefined" && ["Graffiticode"] || document.location.hostname.split(/[.]/g);
   const title = hostnameParts.length === 1 && hostnameParts[0] || hostnameParts[hostnameParts.length - 2];
+  console.log("getTitle() title=" + title)
   switch (title) {
   case "questioncompiler":
     return "QuestionCompiler";
