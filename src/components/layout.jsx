3a96105e58ad128cf6ba31c9e307c490151d8b91
Jeff Dyer <jeffdyer@acm.org>
2023-10-18 06:44:03 -0700

    nav order

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -32,8 +32,8 @@ const navigation = [
   { name: 'Languages', href: '/languages', current: false },
   { name: 'Tasks', href: '/tasks', current: false },
   { name: 'Compiles', href: '/compiles', current: false },
-  { name: 'Explorer', href: '/explorer', current: false },
   { name: 'Specs', href: '/specs', current: false },
+  { name: 'Explorer', href: '/explorer', current: false },
   { name: 'Settings', href: '/settings', current: false },
 ]
 
