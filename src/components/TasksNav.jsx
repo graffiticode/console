517833781d2bb25e386705c12b048d61ed42aa7a
Jeff Dyer <jeffdyer@acm.org>
2024-03-02 15:08:14 -0800

    Add popup menu

--- src/components/TasksNav.jsx
+++ src/components/TasksNav.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Disclosure } from '@headlessui/react';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 import { EllipsisVerticalIcon } from '@heroicons/react/16/solid';
@@ -7,6 +7,64 @@ const sliceName = name => name.slice(17).slice(0,27);
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+//import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+//import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+
+function EllipsisMenu() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="flex items-center mt-1 text-gray-400 hover:text-gray-600">
+          <span className="sr-only">Open options</span>
+          <EllipsisVerticalIcon className="h-4" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Rename
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-xs'
+                  )}
+                >
+                  Hide
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
 }
 
 const scrubTitle = title => title.indexOf("|") >= 0 && title.slice(title.indexOf("|") + 1).trim();
@@ -108,12 +166,7 @@ export default function TasksNav({ setId, setTask, tasks }) {
                       </button>
                       {
                         item.current &&
-                          <button onClick={() => alert("clicked")}>
-                            <EllipsisVerticalIcon
-                              className="pt-0 h-4 fill-gray-500"
-                            />
-                          </button> ||
-                          <div />
+                          <EllipsisMenu />
                       }
                       </div>
                   ) : (
@@ -148,12 +201,7 @@ export default function TasksNav({ setId, setTask, tasks }) {
                             </Disclosure.Button>
                             {
                               item.current &&
-                                <button onClick={() => alert("clicked")}>
-                                  <EllipsisVerticalIcon
-                                    className="pt-0 h-4 fill-gray-500"
-                                  />
-                                </button> ||
-                                <div />
+                                <EllipsisMenu />
                             }
                           </div>
                           <Disclosure.Panel as="ul" className="mt-1 px-2">
@@ -181,12 +229,7 @@ export default function TasksNav({ setId, setTask, tasks }) {
                                   </button>
                                   {
                                     subItem.current &&
-                                      <button onClick={() => alert("clicked")}>
-                                        <EllipsisVerticalIcon
-                                          className="pt-0 h-4 fill-gray-500"
-                                        />
-                                      </button> ||
-                                      <div />
+                                      <EllipsisMenu />
                                   }
                                 </div>
                               </li>
