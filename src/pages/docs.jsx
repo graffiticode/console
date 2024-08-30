9899aaa96ca4daae274d96f6e7f2062299633631
Jeff Dyer <jeffdyer@acm.org>
2024-08-30 11:59:00 -0700

    s/spec/docs

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -5,7 +5,8 @@ import { getBaseUrlForApi } from "../lib/api";
 
 export default function Spec({ language }) {
   const langId = language.name.slice(1);
-  const src = `${getBaseUrlForApi()}/L${langId}/spec.html`;
+  const src = `${getBaseUrlForApi()}/L${langId}/docs.html`;
+  console.log("Spec() src=" + src);
   useEffect(() => {
     document.title = getTitle();
   }, []);
