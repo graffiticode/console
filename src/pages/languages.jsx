90181c5eaa93a3998effbafad05941a7fe2f4178
Jeff Dyer <jeffdyer@acm.org>
2025-02-25 10:51:53 -0800

    Update list of languages

--- src/pages/languages.jsx
+++ src/pages/languages.jsx
@@ -60,7 +60,7 @@ function LanguageList({ language, setLanguage }) {
   return (
     <div>
       <h2 className="text-sm font-medium text-gray-500">Pinned Languages</h2>
-      <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
+      <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
         {languages.map((language) => (
           <Link
             key={language.name}
