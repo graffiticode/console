07db1935874303dac37830c675b7ac5415eaed68
Jeff Dyer <jeffdyer@acm.org>
2025-04-11 11:12:14 -0700

    Reverse meaning of icons in help panel

--- src/components/HelpPanel.jsx
+++ src/components/HelpPanel.jsx
@@ -283,9 +283,9 @@ export const HelpPanel = ({
                           setCode(codeFromHelp);
                         }
                       }}
-                      title="Copy to Code Panel"
+                      title="Copy to Code"
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 transform rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                       </svg>
                     </button>
@@ -306,10 +306,10 @@ export const HelpPanel = ({
                           ));
                         }
                       }}
-                      title="Copy from Code Panel"
+                      title="Copy from Code"
                       disabled={!code}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 transform rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                       </svg>
                     </button>
