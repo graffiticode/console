fb3123789805731715b518eaf0ebc9fd261935ea
Jeff Dyer <jeffdyer@acm.org>
2026-01-13 12:23:57 -0800

    Update google sign-in host name

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 // The server-side scripts use 'graffiticode-app' as the project ID
 export const apiFirebaseConfig = {
   apiKey: "AIzaSyAoVuUNi8ElnS7cn6wc3D8XExML-URLw0I",
-  authDomain: "graffiticode.firebaseapp.com",
+  authDomain: "graffiticode.org",
   databaseURL: "https://graffiticode.firebaseio.com",
   projectId: "graffiticode",
   storageBucket: "graffiticode.appspot.com",
